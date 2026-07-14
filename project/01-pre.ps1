04e1b428b9069e10485a92e5eb8b2644a39620f2
Roland Smith <rsmith@xs4all.nl>
2026-07-14 21:24:36 +0200

    Try and improve preprocessor and solver scripts.

--- project/01-pre.ps1
+++ project/01-pre.ps1
@@ -9,6 +9,14 @@ Runs the Calculix preprocessor.
 Calls the CalculiX preprocessor “cgx” to process the input file “pre.fbd”
 
 #>
-cgx -bg pre.fbd
-Get-Content *CF*.sur | Set-Content ties.sur
-Remove-Item -Path "*CF*.sur" -ErrorAction SilentlyContinue
+$preprocfile="pre.fbd"
+if (!(Test-Path $preprocfile)) {
+    Write-Warning "File $preprocfile not found! Exiting."
+    exit 1
+}
+cgx -bg $preprocfile
+if (Test-Path *CF*.sur -PathType Leaf) {
+    Write-Information -MessageData "Combining DCF and ICF files into ties.sur."
+    Get-Content *CF*.sur | Set-Content ties.sur
+    Remove-Item -Path "*CF*.sur" -ErrorAction SilentlyContinue
+}
--- project/02-solve.ps1
+++ project/02-solve.ps1
@@ -12,8 +12,14 @@ Afterwards it cleans out some temporary files.
 The result data can be found in “job.frd” and “job.dat”.
 
 #>
+$solverfile="job.inp"
+if (!(Test-Path $solverfile -PathType Leaf)) {
+    Write-Warning "File $solverfile not found! Exiting."
+    exit 1
+}
+# Run the solver, copy output to “job.log”.
 ccx -i job | Tee-Object -FilePath "job.log"
-# Clean up
+# Clean up.
 Remove-Item -Path "*.12d" -ErrorAction SilentlyContinue
 Remove-Item -Path "*.sta" -ErrorAction SilentlyContinue
 Remove-Item -Path "*.cvg" -ErrorAction SilentlyContinue
